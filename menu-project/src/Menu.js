23daa5b31ec2e079fe71b1c2ccac9998871429a8
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-14 17:17:57 +0200

    components

--- menu-project/src/Menu.js
+++ menu-project/src/Menu.js
@@ -5,10 +5,17 @@ const Menu = ({ items }) => {
   return (
     <div className="section-container">
       {items.map((menuItem) => {
-        const { id, title, img, desc } = menuItem;
+        const { id, title, img, desc, price } = menuItem;
         return (
           <article key={id} className="menu-item">
             <img src={img} alt={title} className="photo" />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">{price} €</h4>
+              </header>
+              <p className="item-text">{desc}</p>
+            </div>
           </article>
         );
       })}
